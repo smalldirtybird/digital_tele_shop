ac4903fff346c42ed099c5ba9019542dea25719f
smalldirtybird <smalldirtybird@gmail.com>
2022-07-15 16:58:24 +0300

    Send photo to user

--- elastic_path_api.py
+++ elastic_path_api.py
@@ -5,10 +5,12 @@ from dotenv import load_dotenv
 import requests
 
 
-def get_elastic_path_authorization_token(client_id, client_secret):
+def get_authorization_token():
     base_url = 'https://api.moltin.com/'
     path = 'oauth/access_token/'
     url = os.path.join(base_url, path)
+    client_id = os.environ['ELASTIC_PATH_CLIENT_ID']
+    client_secret = os.environ['ELASTIC_PATH_CLIENT_SECRET']
     payload = {
         'client_id': client_id,
         'client_secret': client_secret,
@@ -19,14 +21,40 @@ def get_elastic_path_authorization_token(client_id, client_secret):
     return response.json()['access_token']
 
 
-def get_elastic_path_products(elastic_path_auth_token):
+def get_products(elastic_path_auth_token):
     base_url = 'https://api.moltin.com/'
     path = 'v2/products/'
     url = os.path.join(base_url, path)
     headers = {'Authorization': f'Bearer {elastic_path_auth_token}'}
     response = requests.get(url, headers=headers)
     response.raise_for_status()
-    return response.json()
+    return response.json()['data']
+
+
+def get_product_details(elastic_path_auth_token, product_id):
+    base_url = 'https://api.moltin.com/'
+    path = 'v2/products/'
+    url = os.path.join(base_url, path, product_id)
+    headers = {'Authorization': f'Bearer {elastic_path_auth_token}'}
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    product = response.json()['data']
+    product_name = product['name']
+    product_price = product['meta']['display_price']['with_tax']['formatted']
+    product_stock = product['meta']['stock']['level']
+    product_description = product['description']
+    product_image_id = product['relationships']['main_image']['data']['id']
+    return product_name, product_price, product_stock, product_description, product_image_id
+
+
+def get_product_image_link(elastic_path_auth_token, file_id):
+    base_url = 'https://api.moltin.com/'
+    path = 'v2/files/'
+    url = os.path.join(base_url, path, file_id)
+    headers = {'Authorization': f'Bearer {elastic_path_auth_token}'}
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    return response.json()['data']['link']['href']
 
 
 def add_product_to_cart(elastic_path_auth_token, cart_name, product_id):
@@ -60,18 +88,15 @@ def main():
     load_dotenv()
     client_id = os.environ['ELASTIC_PATH_CLIENT_ID']
     client_secret = os.environ['ELASTIC_PATH_CLIENT_SECRET']
-    elastic_path_auth_token = get_elastic_path_authorization_token(
-        client_id,
-        client_secret,
-    )
+    elastic_path_auth_token = get_authorization_token()
     # print(elastic_path_auth_token)
-    # pprint(get_elastic_path_products(elastic_path_auth_token))
-    pprint(add_product_to_cart(
-        elastic_path_auth_token,
-        'test_cart_2',
-        '91d918d1-f992-4c28-9149-6c7daa965c9c'
-    ))
-    pprint(get_cart_items(elastic_path_auth_token, 'test_cart_2'))
+    pprint(get_products(elastic_path_auth_token))
+    # pprint(add_product_to_cart(
+    #     elastic_path_auth_token,
+    #     'test_cart_2',
+    #     '91d918d1-f992-4c28-9149-6c7daa965c9c'
+    # ))
+    # pprint(get_cart_items(elastic_path_auth_token, 'test_cart_2'))
 
 
 if __name__ == '__main__':
--- tg_bot.py
+++ tg_bot.py
@@ -1,27 +1,41 @@
+import argparse
 import os
+import urllib
+from functools import partial
+from textwrap import dedent
 
 import redis
+import requests
 from dotenv import load_dotenv
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Filters, Updater
-from telegram.ext import CallbackQueryHandler, CommandHandler, MessageHandler
+from telegram.ext import (CallbackQueryHandler, CommandHandler, Filters,
+                          MessageHandler, Updater)
+
+from elastic_path_api import (get_authorization_token, get_product_details,
+                              get_product_image_link, get_products)
 
 _database = None
 
 
 def start(bot, update):
-    keyboard = [[InlineKeyboardButton("Option 1", callback_data='1')],
-                [InlineKeyboardButton("Option 2", callback_data='2')],
-                [InlineKeyboardButton("Option 3", callback_data='3')]]
+    ep_authorization_token = get_authorization_token()
+    products = get_products(ep_authorization_token)
+    keyboard = []
+    for number,  product in enumerate(products):
+        product_name = product['name']
+        product_id = product['id']
+        product_button = [InlineKeyboardButton(
+            product_name, callback_data=product_id)]
+        keyboard.append(product_button)
     reply_markup = InlineKeyboardMarkup(keyboard)
     update.message.reply_text('Please choose:', reply_markup=reply_markup)
-    return 'ECHO'
+    return 'HANDLE_MENU'
 
 
 def button(bot, update):
     query = update.callback_query
 
-    bot.edit_message_text(text="Selected option: {}".format(query.data),
+    bot.edit_message_text(text="Product id: {}".format(query.data),
                           chat_id=query.message.chat_id,
                           message_id=query.message.message_id)
 
@@ -30,6 +44,57 @@ def echo(bot, update):
     users_reply = update.message.text
     update.message.reply_text(users_reply)
     return 'ECHO'
+
+
+def download_image(url, path, params=None):
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    with open(path, 'wb') as file:
+        file.write(response.content)
+
+
+def get_image_extension(url):
+    parsed_url = urllib.parse.urlsplit(url, scheme='', allow_fragments=True)
+    filepath = urllib.parse.unquote(parsed_url[2],
+                                    encoding='utf-8', errors='replace')
+    path, extension = os.path.splitext(filepath)
+    return extension
+
+
+def handle_menu(bot, update, image_folder_path):
+    query = update.callback_query
+    product_id = query.data
+    ep_authorization_token = get_authorization_token()
+    product_name, product_price, product_stock, product_description,\
+        product_image_id = get_product_details(
+            ep_authorization_token,
+            product_id,
+        )
+    product_image_link = get_product_image_link(
+        ep_authorization_token,
+        product_image_id,
+    )
+    image_file_extension = get_image_extension(product_image_link)
+    image_filepath = os.path.join(
+        image_folder_path,
+        f'{product_name}{image_file_extension}',
+    )
+    download_image(product_image_link, image_filepath)
+    chat_id = update['callback_query']['message']['chat']['id']
+    message_id = update['callback_query']['message']['message_id']
+    bot.delete_message(
+        chat_id=chat_id,
+        message_id=message_id,
+    )
+    message = dedent(f'''
+        {product_name}\n
+        {product_price} for one piece\n
+        {product_stock} pieces available on stock\n
+        {product_description}''')
+    with open(image_filepath, 'rb') as image_file:
+        bot.send_photo(chat_id, image_file, caption=message)
+    os.remove(image_filepath)
+    return 'START'
 
 
 def handle_users_reply(bot, update):
@@ -48,7 +113,8 @@ def handle_users_reply(bot, update):
         user_state = db.get(chat_id).decode('utf-8')
     states_functions = {
         'START': start,
-        'ECHO': echo
+        'ECHO': echo,
+        'HANDLE_MENU': handle_menu
     }
     state_handler = states_functions[user_state]
     # Если вы вдруг не заметите, что python-telegram-bot перехватывает ошибки.
@@ -67,16 +133,37 @@ def get_database_connection():
         database_password = os.environ['DB_PASSWORD']
         database_host = os.environ['DB_HOST']
         database_port = os.environ['DB_PORT']
-        _database = redis.Redis(host=database_host, port=database_port, password=database_password)
+        _database = redis.Redis(
+            host=database_host,
+            port=database_port,
+            password=database_password,
+        )
     return _database
 
 
-if __name__ == '__main__':
+def get_image_folder_path_arguments():
+    parser = argparse.ArgumentParser(
+        description='Seafood shop in Telegram.')
+    parser.add_argument(
+        '-dir', '--directory', default='images/',
+        help='Image folder path.')
+    args = parser.parse_args()
+    return args.directory
+
+
+def main():
     load_dotenv()
+    image_folder = get_image_folder_path_arguments()
+    os.makedirs(image_folder, exist_ok=True)
     token = os.environ['TELEGRAM_BOT_TOKEN']
     updater = Updater(token)
     dispatcher = updater.dispatcher
-    dispatcher.add_handler(CallbackQueryHandler(button))
+    handling_menu = partial(handle_menu, image_folder_path=image_folder)
+    dispatcher.add_handler(CallbackQueryHandler(handling_menu))
     dispatcher.add_handler(MessageHandler(Filters.text, handle_users_reply))
     dispatcher.add_handler(CommandHandler('start', handle_users_reply))
     updater.start_polling()
+
+
+if __name__ == '__main__':
+    main()
